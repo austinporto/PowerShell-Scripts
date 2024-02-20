640a55440b6008a0907902ab1552c67fc145f5e4
austinporto <77503297+austinporto@users.noreply.github.com>
2024-02-20 10:47:08 -0500

    Add files via upload

---
+++ DisableOWA.ps1
@@ -0,0 +1,13 @@
+﻿#BBE SCRIPT AUSTIN
+
+# Use this to remove Outlook Web (OWA) from user profiles
+
+Connect-ExchangeOnline 
+
+Get-Mailbox -ResultSize Unlimited | ForEach-Object {
+    Set-CASMailbox -Identity $_.Identity -OWAEnabled $false
+
+}
+
+Disconnect-ExchangeOnline -Confirm:$false
+
